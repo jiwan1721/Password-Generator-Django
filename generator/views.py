4974246b704ba415968720a7967f9295dce884fa
Jiwan <chandjiwn9@gmail.com>
2022-06-21 23:27:10 +0545

    passowrd generator

---
+++ generator/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random,string
+
+# Create your views here.
+def randpassword():
+        number = string.digits
+        punc=string.punctuation
+        randomPassword = list(string.ascii_letters+number+punc)
+
+        length_password = random.randrange(8,15)
+        random.shuffle(randomPassword)
+        password = ''.join(random.choice(randomPassword)for i in range(length_password))
+        last_value=password[-1]
+        if last_value in number or last_value in punc :
+            print('------')
+            return randpassword()
+        else:
+            return password
+def home(request):
+    return render(request, 'generator/home.html')
+
+def about(request):
+    return render(request, 'generator/about.html')
+
+def password(request):
+    password=randpassword()
+
+    return render(request, 'generator/password.html', {'password':password})
